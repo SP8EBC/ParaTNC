44ed8c6f3828eb74f8cf5d7b6683b625fdfbb7aa
Mateusz Lubecki <sp8ebc@interia.pl>
2021-06-13 21:34:06 +0200

    stm32l471 startup assembling fixed

--- STM32L476_ParaMETEO/system/src/cmsis/stm32l4xx/subdir.mk
+++ STM32L476_ParaMETEO/system/src/cmsis/stm32l4xx/subdir.mk
@@ -7,9 +7,16 @@ C_SRCS += \
 ../system/src/cmsis/stm32l4xx/stm32l4xx_it.c \
 ../system/src/cmsis/stm32l4xx/system_stm32l4xx.c 
 
+S_UPPER_SRCS += \
+../system/src/cmsis/stm32l4xx/startup_stm32l471xx.S 
+
 OBJS += \
+./system/src/cmsis/stm32l4xx/startup_stm32l471xx.o \
 ./system/src/cmsis/stm32l4xx/stm32l4xx_it.o \
 ./system/src/cmsis/stm32l4xx/system_stm32l4xx.o 
+
+S_UPPER_DEPS += \
+./system/src/cmsis/stm32l4xx/startup_stm32l471xx.d 
 
 C_DEPS += \
 ./system/src/cmsis/stm32l4xx/stm32l4xx_it.d \
@@ -17,6 +24,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
+system/src/cmsis/stm32l4xx/%.o: ../system/src/cmsis/stm32l4xx/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants  -g3 -x assembler-with-cpp -DDEBUG -DUSE_FULL_LL_DRIVER -DSTM32L471xx -USTM32F10X_MD_VL -I"../include" -I"../system/include" -I"../system/include/cmsis/stm32l4xx" -I"../system/include/cmsis/stm32l4xx/device" -I"../system/include/stm32l4-hal-driver" -I"../system/include/stm32l4-hal-driver/Legacy" -Wa,-adhlns="$@.lst" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 system/src/cmsis/stm32l4xx/%.o: ../system/src/cmsis/stm32l4xx/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross ARM C Compiler'
