1e23ed3af8ad9c8d1f1e506d5cc70f4f54eb8cbe
Mateusz Lubecki <sp8ebc@interia.pl>
2024-07-09 21:54:42 +0200

    macro to create pointers for each logging instance

--- include/etc/nvm_configuration.h
+++ include/etc/nvm_configuration.h
@@ -1,6 +1,11 @@
 #ifndef A829659A_5A6C_450A_B678_DF44F9CAFB9B
 #define A829659A_5A6C_450A_B678_DF44F9CAFB9B
 
+
+#define NVM_EVENT_LOGGING_TARGETS(ENTRY)														\
+			/* TargetName,	NonPtrBasedWriteFunction,	AreaStartAddress,			AreaEndAddress,				EraseFunction,		PgmingEnableFunction,	WaitPgmCompleteFunction,	PgmingDisableFunction,	MinimumSeverityLevel,	PointerBasedAccess */	\
+	ENTRY(	Flash,			NULL,						MEMORY_MAP_EVENT_LOG_START,	MEMORY_MAP_EVENT_LOG_END,	FLASH_ErasePage,	NVM_CONFIG_ENABLE_PGM,	WAIT_FOR_PGM_COMPLETION,	NVM_CONFIG_DISABLE_PGM,	1,						true)					\
+
 #define WAIT_FOR_PGM_COMPLETION			\
 	while (1) {\
 		flash_status = FLASH_GetBank1Status();				\
