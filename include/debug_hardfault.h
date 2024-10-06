bdd000f892caefb5fcf28bbb3980c6f291b67b56
Mateusz Lubecki <sp8ebc@interia.pl>
2024-10-06 10:04:18 +0200

    hardfault logger

--- include/debug_hardfault.h
+++ include/debug_hardfault.h
@@ -8,6 +8,8 @@
 #ifndef DEBUG_HARDFAULT_H_
 #define DEBUG_HARDFAULT_H_
 
+#ifdef STM32L471xx
+
 #include <stdint.h>
 
 #include "memory_map.h"
@@ -16,25 +18,40 @@
 ///	X-MACROS
 /// ==================================================================================================
 
-#define DEBUG_STACKFRAME_STORE(stackpointer, source)                                                      \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R0) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R0);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R1) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R1);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R2) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R2);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R3) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R3);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R12) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R12);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_LR) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_LR);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_PC) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_PC);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_XPSR) = *((volatile uint32_t*)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_XPSR);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_SOURCE) = ((uint32_t)source & 0xFF);   \
-    *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_CFSR) = SCB->CFSR;   \
+/**
+ *
+ */
+#define DEBUG_STACKFRAME_STORE(stackpointer, source)                                     \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R0) =     \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R0);      \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R1) =     \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R1);      \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R2) =     \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R2);      \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R3) =     \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R3);      \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R12) =    \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_R12);     \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_LR) =     \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_LR);      \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_PC) =     \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_PC);      \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_XPSR) =   \
+		*((volatile uint32_t *)stackpointer + (uint32_t)DEBUG_HARDFAULT_OFFSET_XPSR);    \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_SOURCE) = \
+		((uint32_t)source & 0xFF);                                                       \
+	*((uint32_t *)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_CFSR) = SCB->CFSR;   \
 
-#define DEBUG_STACKFRAME_CHECKSUM						                                            \
-																									\
-    uint32_t sum = 0;                                                                               \
-    for (int i = 0; i < DEBUG_HARDFAULT_OFFSET_CHECKSUM; i++) {                                     \
-        sum += *((uint32_t*)MEMORY_MAP_SRAM1_HFAULT_LOG_START + i);                                 \
-    }                                                                                               \
-    *((uint32_t*)MEMORY_MAP_SRAM1_HFAULT_LOG_START + DEBUG_HARDFAULT_OFFSET_CHECKSUM) = sum + 9u;   \
+/**
+ *
+ */
+#define DEBUG_STACKFRAME_CHECKSUM                                                    \
+                                                                                     \
+	uint32_t sum = 0;                                                                \
+	for (int i = 0; i < DEBUG_HARDFAULT_OFFSET_CHECKSUM; i++) {                      \
+		sum += (*((uint32_t *)MEMORY_MAP_SRAM1_HFAULT_LOG_START + i) & 0x7FFFFFFFu); \
+	}                                                                                \
+	*((uint32_t *)MEMORY_MAP_SRAM1_HFAULT_LOG_START + DEBUG_HARDFAULT_OFFSET_CHECKSUM) = sum + 9u;  \
 
 /// ==================================================================================================
 ///	GLOBAL MACROS
@@ -67,6 +84,20 @@
 ///	GLOBAL TYPES
 /// ==================================================================================================
 
+typedef struct debug_hardfault_postmortem_stackframe_t {
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r12;
+    uint32_t lr;
+    uint32_t pc;
+    uint32_t xpsr;
+    uint32_t source;
+    uint32_t cfsr;
+
+} debug_hardfault_postmortem_stackframe_t;
+
 /// ==================================================================================================
 ///	GLOBAL VARIABLES
 /// ==================================================================================================
@@ -77,6 +108,32 @@ extern volatile uint32_t debug_hardfault_stack_pointer_value;
 ///	GLOBAL FUNCTIONS
 /// ==================================================================================================
 
+/**
+ * Checks if SRAM1 noinit are contains any data from hard fault or any exception
+ * @return zero if no postmortem snapshot are stored, non-zero value otherwise
+ */
+uint8_t debug_hardfault_check_have_postmortem(void);
 
+/**
+ * Delete collected postmortem data
+ */
+void debug_hardfault_delete_postmortem(void);
+
+/**
+ * 
+ */
+uint8_t debug_hardfault_get_postmortem(debug_hardfault_postmortem_stackframe_t * out);
+
+/**
+ * Assemble string representation of stored postmortem data
+ * @param in postmortem information restored from noinit area
+ * @param output_string pointer to character array where string will be assembled into
+ * @param output_string_size size of output buffer
+ * @return size of assembled string
+ */
+uint16_t debug_hardfault_assemble_info_string (debug_hardfault_postmortem_stackframe_t *in,
+											   char *output_string, uint16_t output_string_size);
+
+#endif
 
 #endif /* DEBUG_HARDFAULT_H_ */
--- src/debug_hardfault.c
+++ src/debug_hardfault.c
@@ -6,6 +6,13 @@
  */
 
 #include "debug_hardfault.h"
+#include "variant.h"
+
+#include <stdio.h>
+
+#ifdef STM32L471xx
+
+#include <stm32l4xx.h>
 
 /// ==================================================================================================
 ///	LOCAL DATA TYPES
@@ -29,3 +36,128 @@ volatile uint32_t debug_hardfault_stack_pointer_value = 0;
 ///	GLOBAL FUNCTIONS
 /// ==================================================================================================
 
+/**
+ * Checks if SRAM1 noinit are contains any data from hard fault or any exception
+ * @return zero if no postmortem snapshot are stored, non-zero value otherwise
+ */
+uint8_t debug_hardfault_check_have_postmortem (void)
+{
+	uint32_t sum = 0;
+	const uint32_t sum_from_postmortem =
+		*((uint32_t *)MEMORY_MAP_SRAM1_HFAULT_LOG_START + DEBUG_HARDFAULT_OFFSET_CHECKSUM);
+
+	for (int i = 0; i < DEBUG_HARDFAULT_OFFSET_CHECKSUM; i++) {
+		sum += (*((uint32_t *)MEMORY_MAP_SRAM1_HFAULT_LOG_START + i) & 0x7FFFFFFFu);
+	}
+
+	if (sum == sum_from_postmortem) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+/**
+ * Delete collected postmortem data
+ */
+void debug_hardfault_delete_postmortem(void) {
+
+}
+
+/**
+ * Checks if SRAM1 noinit are contains any data from hard fault or any exception
+ * @return zero if no postmortem snapshot are stored, non-zero value otherwise
+ */
+uint8_t debug_hardfault_get_postmortem(debug_hardfault_postmortem_stackframe_t * out) {
+
+    if (variant_validate_is_within_ram(out) != 0) {
+        if (debug_hardfault_check_have_postmortem() != 0) {
+            out->r0 = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R0);
+            out->r1 = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R1);
+            out->r2 = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R2);
+            out->r3 = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R3);
+            out->r12 = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_R12);
+            out->lr = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_LR);
+            out->pc = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_PC);
+            out->xpsr = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_XPSR);
+            out->source = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_SOURCE);
+            out->cfsr = *((uint32_t*)(MEMORY_MAP_SRAM1_HFAULT_LOG_START) + DEBUG_HARDFAULT_OFFSET_CFSR);
+
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    else {
+        return 0;
+    }
+}
+
+/**
+ * Assemble string representation of stored postmortem data
+ * @param in postmortem information restored from noinit area
+ * @param output_string pointer to character array where string will be assembled into
+ * @param output_string_size size of output buffer
+ * @return size of assembled string
+ */
+uint16_t debug_hardfault_assemble_info_string (debug_hardfault_postmortem_stackframe_t *in,
+											   char *output_string, uint16_t output_string_size)
+{
+
+	if (variant_validate_is_within_ram(in) == 0) {
+        return 0;
+    }
+
+    if (variant_validate_is_within_ram(output_string) == 0) {
+        return 0;
+    }
+
+    uint16_t ouput_ln = 0;
+
+	switch (in->source) {
+	case DEBUG_HARDFAULT_SOURCE_HFLT: {
+		ouput_ln = snprintf (output_string, output_string_size,
+							 "[!][HF][LR: 0x%lX][SP: 0x%lX][CFSR: 0x%lX][R0: 0x%lX][R1: 0x%lX][R2: "
+							 "0x%lX][R3: 0x%lX]",
+							 in->lr, in->pc, in->cfsr, in->r0, in->r1, in->r2, in->r3);
+		break;
+	}
+
+	case DEBUG_HARDFAULT_SOURCE_USAGEFLT: {
+		ouput_ln = snprintf (output_string, output_string_size,
+							 "[!][US][LR: 0x%lX][SP: 0x%lX][CFSR: 0x%lX][R0: 0x%lX][R1: 0x%lX][R2: "
+							 "0x%lX][R3: 0x%lX]",
+							 in->lr, in->pc, in->cfsr, in->r0, in->r1, in->r2, in->r3);
+
+		break;
+	}
+
+	case DEBUG_HARDFAULT_SOURCE_BUSFLT: {
+		ouput_ln = snprintf (output_string, output_string_size,
+							 "[!][BU][LR: 0x%lX][SP: 0x%lX][CFSR: 0x%lX][R0: 0x%lX][R1: 0x%lX][R2: "
+							 "0x%lX][R3: 0x%lX]",
+							 in->lr, in->pc, in->cfsr, in->r0, in->r1, in->r2, in->r3);
+
+		break;
+	}
+
+	case DEBUG_HARDFAULT_SOURCE_MMUFLT: {
+		ouput_ln = snprintf (output_string, output_string_size,
+							 "[!][MM][LR: 0x%lX][SP: 0x%lX][CFSR: 0x%lX][R0: 0x%lX][R1: 0x%lX][R2: "
+							 "0x%lX][R3: 0x%lX]",
+							 in->lr, in->pc, in->cfsr, in->r0, in->r1, in->r2, in->r3);
+
+		break;
+	}
+
+	default: {
+		break;
+	}
+	}
+
+	return ouput_ln;
+}
+
+#endif
