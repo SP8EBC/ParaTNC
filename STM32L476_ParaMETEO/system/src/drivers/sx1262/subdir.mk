80d9e599870fb450be32a9677660e8a169d35dba
Mateusz Lubecki <sp8ebc@interia.pl>
2025-01-10 18:12:57 +0100

    makefile

---
+++ STM32L476_ParaMETEO/system/src/drivers/sx1262/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../system/src/drivers/sx1262/sx1262_internals.c \
+../system/src/drivers/sx1262/sx1262_modes.c 
+
+OBJS += \
+./system/src/drivers/sx1262/sx1262_internals.o \
+./system/src/drivers/sx1262/sx1262_modes.o 
+
+C_DEPS += \
+./system/src/drivers/sx1262/sx1262_internals.d \
+./system/src/drivers/sx1262/sx1262_modes.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/drivers/sx1262/%.o: ../system/src/drivers/sx1262/%.c system/src/drivers/sx1262/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wuninitialized -Wall -Wextra -Wlogical-op -Waggregate-return -g3 -DDEBUG -DHI_SPEED -DUSE_FULL_LL_DRIVER -DSTM32L471xx -DPARAMETEO -USTM32F10X_MD_VL -UPARATNC_HWREV_A -UPARATNC_HWREV_B -UPARATNC_HWREV_C -I"../include" -I"../include/configuration_nvm" -I"../include/etc" -I"../system/include/tiny-aes" -I"../system/include/aprs" -I"../system/include" -I"../system/include/tm" -I"../system/include/cmsis/stm32l4xx" -I"../system/include/cmsis/stm32l4xx/device" -I"../system/include/stm32l4-hal-driver" -I"../system/include/stm32l4-hal-driver/Legacy" -std=gnu11 -Wunused-function -Wall -Wa,-adhlns="$@.lst" -fstack-usage -fdump-rtl-dfinish -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
